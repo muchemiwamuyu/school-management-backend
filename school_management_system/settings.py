c778a5003f55e73e5d40b265066c90f107897052
Edwin muchemi <muchemiedwin68@gmail.com>
2025-06-16 23:09:12 +0300

    updated some config files

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -148,12 +148,12 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 # For Gmail (use App Password)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'muchemiedwin68@gmail.com'
-EMAIL_HOST_PASSWORD = 'alwf yvtr qbnl eswz'  # Not your normal password!
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # Your email address
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Not your normal password!
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
